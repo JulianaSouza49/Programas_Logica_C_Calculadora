0c5f74e98dff2bf070f13559257f35fe27997d0a
JulianaSouza49 <lj.m.msouza@gmail.com>
2025-12-02 16:45:21 -0300

    Soma

---
+++ somardoisnumeros.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(){
+    int x;
+    int y;
+    int soma;
+    printf("Digite o valor de x: ");
+    scanf("%d",&x);
+    printf("Digite o valor de y: ");
+    scanf("%d",&y);
+    soma = x + y;
+    printf("A soma eh: %d",soma);
+return 0;
+}
+
